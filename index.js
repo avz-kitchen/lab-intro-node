3baabfe9db51d8c95e3a8addaace3604ea6cc46e
AValenzue <zuellagraphy@gmail.com>
2024-04-29 18:06:12 +0200

    solved labs

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.items.forEach((element) => {
+      this.length++;
+    })
 
-  get(pos) {}
+    return this.items;
+  }
 
-  max() {}
+  get(pos) {
+    let myList = this.items;
 
-  min() {}
+    if (pos < 0 || pos >= myList.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    let max = Math.max(...this.items);
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return max;
+  }
 
-  avg() {}
+  min() {
+    let min = Math.min(...this.items);
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return min;
+  }
+
+  sum() {
+    let sum = 0;
+    this.items.forEach((element) => {
+      sum += element;
+
+    });
+
+    if (this.length === 0) {
+      return 0
+    }
+    return sum
+
+  }
+
+  avg() {
+    let myList = this.items;
+    let sum = 0;
+    for(let i = 0; i  < myList.length; i++) {
+      sum += myList[i];
+    }
+    if(myList.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return sum / myList.length;
+  }
 }
 
 module.exports = SortedList;
